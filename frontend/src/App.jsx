5c43ca1cec0553798e06bd4248e8cd02f3ec7b53
Monikanto <manikurmi14@gmail.com>
2025-12-30 15:29:36 +0530

    Prepare backend for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-// import Home from "./pages/Home";
-
-// function App() {
-//   return <Home />;
-// }
-
-// export default App;
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-red-500 text-white text-4xl flex items-center justify-center">
-      Tailwind Test
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="min-h-screen bg-red-500 text-white text-4xl flex items-center justify-center">
+//       Tailwind Test
+//     </div>
+//   );
+// }
+
+// export default App;
